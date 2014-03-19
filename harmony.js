bc1a6abe9cfccaee63d8899d960a62eaa72aa21b
Mat Gadd <source@catchall.drarok.com>
2014-03-19 12:33:50 +0000

    Refactor most code to a Harmony class.

--- harmony.js
+++ harmony.js
@@ -1,87 +1,12 @@
 var http = require('http');
-var url = require('url');
-var mysql = require('mysql');
-var MySQL = require('./src/Server/MySQL');
-var SQLite = require('./src/Server/SQLite');
 
-var collections = {
-    support: {
-        servers: {
-            mysql: new MySQL('127.0.0.1', 'root', '', {database: 'test'}),
-            sqlite: new SQLite('/tmp/harmony.sqlite3')
-        },
-        objects: {
-            customers: ['mysql', 'sqlite']
-        }
-    }
-};
+var Harmony = require('./src/Harmony');
+var harmonyInstance = new Harmony(__dirname + '/config/collections.json');
 
-function handleRequest(req, res) {
-    var parsed = parseRequest(req);
+// This function is required to ensure 'this' is valid in the instance.
+var server = http.createServer(function (req, res) {
+    harmonyInstance.handleRequest(req, res);
+});
 
-    if (! (parsed.collection in collections)) {
-        res.writeHead(404, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({error: 'not_found', reason: 'no_such_collection'}));
-        return;
-    }
-
-    var collection = collections[parsed.collection];
-
-    if (! (parsed.object in collection.objects)) {
-        res.writeHead(404, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({error: 'not_found', reason: 'no_such_object'}));
-        return;
-    }
-
-    var servers = collection.servers;
-    var objectServers = collection.objects[parsed.object];
-
-    res.writeHead(200, {'Content-Type': 'application/json'});
-
-    var expected = 0;
-    var actual = 0;
-
-    var results = [];
-
-    var callback = function (name, rows) {
-        for (var i in rows) {
-            var row = {_server: name};
-            for (var k in rows[i]) {
-                row[k] = rows[i][k];
-            }
-            results.push(row);
-        }
-
-        if (expected == ++actual) {
-            res.end(JSON.stringify(results, null, '\t'));
-        }
-    };
-
-    var callbackMaker = function (name, callback) {
-        return function (rows) {
-            callback(name, rows);
-        };
-    };
-
-    for (var i in objectServers) {
-        var name = objectServers[i];
-        var server = servers[name];
-        ++expected;
-        server.getTable(parsed.object, parsed.query, callbackMaker(name, callback));
-    }
-}
-
-function parseRequest(req) {
-    var parsed = url.parse(req.url, true);
-    var pathParts = parsed.pathname.split('/');
-
-    return {
-        collection: pathParts[1],
-        object: pathParts[2],
-        query: parsed.query
-    };
-}
-
-http.createServer(handleRequest).listen(8008, '127.0.0.1');
-
+server.listen(8008, '127.0.0.1');
 console.log('HTTP server running on http://127.0.0.1:8008');
---
+++ src/Harmony.js
@@ -0,0 +1,104 @@
+var url = require('url');
+
+function Harmony(configPath) {
+    this.collections = this.createCollections(configPath);
+}
+
+Harmony.prototype.handleRequest = function (req, res) {
+    var parsed = this.parseRequest(req);
+
+    if (! (parsed.collection in this.collections)) {
+        this.writeNotFoundError(res, 'no_such_collection');
+        return;
+    }
+
+    var collection = this.collections[parsed.collection];
+
+    if (! (parsed.object in collection.objects)) {
+        this.writeNotFoundError(res, 'no_such_object');
+        return;
+    }
+
+    this.dispatch(res, collection, parsed.object, parsed.query);
+};
+
+Harmony.prototype.dispatch = function (res, collection, object, query) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    var expected = 0;
+    var actual = 0;
+
+    var results = [];
+
+    var callback = function (name, rows) {
+        for (var i in rows) {
+            var row = {_server: name};
+            for (var k in rows[i]) {
+                row[k] = rows[i][k];
+            }
+            results.push(row);
+        }
+
+        if (expected == ++actual) {
+            res.end(JSON.stringify(results, null, '\t'));
+        }
+    };
+
+    var callbackMaker = function (name, callback) {
+        return function (rows) {
+            callback(name, rows);
+        };
+    };
+
+    var serverNames = collection.objects[object];
+
+    for (var i in serverNames) {
+        ++expected;
+        var name = serverNames[i];
+        var server = collection.servers[name];
+        server.getTable(object, query, callbackMaker(name, callback));
+    }
+};
+
+Harmony.prototype.writeNotFoundError = function (res, reason) {
+    this.writeError(res, 404, 'not_found', reason);
+};
+
+Harmony.prototype.writeError = function (res, status, error, reason) {
+    res.writeHead(status, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({error: error, reason: reason}));
+};
+
+Harmony.prototype.parseRequest = function (req) {
+    var parsed = url.parse(req.url, true);
+    var pathParts = parsed.pathname.split('/');
+
+    return {
+        collection: pathParts[1],
+        object: pathParts[2],
+        query: parsed.query
+    };
+};
+
+Harmony.prototype.createCollections = function (configPath) {
+    var fs = require('fs');
+    var factory = require('./Server').factory;
+
+    var config = fs.readFileSync(configPath);
+    var json = JSON.parse(config);
+
+    for (var c in json) {
+        var collection = json[c];
+        for (var s in collection.servers) {
+            var server = collection.servers[s];
+            collection.servers[s] = factory(
+                server.type, server.hostname, server.username,
+                server.password, server.options
+            );
+        }
+    }
+
+    return json;
+};
+
+module.exports = Harmony;

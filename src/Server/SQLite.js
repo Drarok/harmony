5b4d464328307d49b8bb7a6f63ecba301eec3e90
Mat Gadd <source@catchall.drarok.com>
2014-03-20 11:02:54 +0000

    Update SQLite drive to match others' style.

--- src/Server/SQLite.js
+++ src/Server/SQLite.js
@@ -6,23 +6,33 @@ function SQLite() {
 
 SQLite.prototype = new Server();
 
-SQLite.prototype.connect = function() {
+SQLite.prototype.connect = function(callback) {
     var sqlite3 = require('sqlite3');
     this.connection = new sqlite3.Database(
         this.hostname,
-        sqlite3.OPEN_READONLY
+        sqlite3.OPEN_READONLY,
+        callback
     );
 };
 
 SQLite.prototype.getTable = function (name, query, callback) {
+    var self = this;
+
     if (! this.connection) {
-        this.connect();
+        this.connect(function (err) {
+            if (err) {
+                self.onError(err);
+                return;
+            }
+
+            self.getTable(name, query, callback);
+        });
     }
 
     var sql = 'SELECT * FROM customers';
     this.connection.all(sql, function (err, rows) {
         if (err) {
-            this.onError(err);
+            self.onError(err);
             return;
         }
 

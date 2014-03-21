a0e78888c837846f39081ebdac744fcb452b0c33
Mat Gadd <source@catchall.drarok.com>
2014-03-21 11:36:57 +0000

    Move boilerplate code to the Server object.

--- src/Server.js
+++ src/Server.js
@@ -10,12 +10,33 @@ Server.factory = function (type, hostname, username, password, options) {
     return new ServerObject(hostname, username, password, options);
 };
 
-Server.prototype.connect = function() {
+Server.prototype.connect = function(callback) {
     throw 'You must override connect() in your Server object.';
 };
 
-Server.prototype.getTable = function (name, query) {
-    throw 'You must override getTable() in your Server object.';
+Server.prototype.isConnected = function () {
+    return !! this.connection;
+};
+
+Server.prototype.getTable = function (name, query, callback) {
+    var self = this;
+
+    if (! this.isConnected()) {
+        this.connect(function (err) {
+            if (err) {
+                self.onError(err);
+                return;
+            }
+
+            self._getTable(name, query, callback);
+        });
+    } else {
+        this._getTable(name, query, callback);
+    }
+};
+
+Server.prototype._getTable = function (name, query, callback) {
+    throw 'You must override _getTable() in your Server object.';
 };
 
 Server.prototype.onError = function (err) {
